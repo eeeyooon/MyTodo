bfbb9c990d987024955e5d7b0d9471752ea8b84f
jiyoon <vywns9978@gmail.com>
2023-09-18 22:42:10 +0900

    [🍡 style] todoItem, todoInput 스타일 적용

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,5 +1,45 @@
+import styled from 'styled-components';
+
 function TodoInput() {
-	return <div>TodoInput</div>;
+	return (
+		<TodoInputBox>
+			<TodoInputFormWrapper>
+				<TodoInputForm type="text" placeholder="Your Task Here..." />
+				<button>
+					<img src={process.env.PUBLIC_URL + '/assets/send.svg'} alt="전송 아이콘" />
+				</button>
+			</TodoInputFormWrapper>
+		</TodoInputBox>
+	);
 }
 
 export default TodoInput;
+
+const TodoInputBox = styled.div`
+	width: 345px;
+	height: 35px;
+	color: rgba(53, 56, 62, 0.5);
+	text-align: center;
+	border-bottom: 2px solid rgba(53, 56, 62, 0.1);
+`;
+
+const TodoInputFormWrapper = styled.div`
+	width: 315px;
+	height: 26px;
+	display: flex;
+	justify-content: space-between;
+	margin: 15px;
+`;
+
+const TodoInputForm = styled.input`
+	border: none;
+	width: 300px;
+	font-size: ${({ theme }) => theme.fontSize['2xl']};
+	font-weight: ${({ theme }) => theme.fontWeight.bold};
+	color: rgba(53, 56, 62, 0.5);
+	outline: none;
+
+	::placeholder {
+		color: rgba(53, 56, 62, 0.5);
+	}
+`;
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
 import TodoItem from '../components/TodoItem';
+import TodoInput from '../components/TodoInput';
 
 function TodoList() {
 	return (
 		<TodoListWrapper>
-			<h1>Todo List</h1>
-			<TodoItem />
-			<TodoItem />
+			<TodoHeaderWrapper>
+				<TodoHeader>Todo List</TodoHeader>
+				<p>User의 to do list입니다.</p>
+			</TodoHeaderWrapper>
+			<TodoItemWrapper>
+				<TodoItem />
+				<TodoItem />
+				<TodoItem />
+			</TodoItemWrapper>
+			<TodoInputWrapper>
+				<TodoInput />
+			</TodoInputWrapper>
 		</TodoListWrapper>
 	);
 }
@@ -20,4 +30,41 @@ const TodoListWrapper = styled.div`
 	display: flex;
 	flex-flow: column nowrap;
 	align-items: center;
+`;
+
+const TodoHeaderWrapper = styled.div`
+	height: 170px;
+	display: flex;
+	align-items: flex-start;
+	flex-flow: column nowrap;
+	margin-left: 28px;
+	margin-top: 72px;
+
+	p {
+		font-weight: ${({ theme }) => theme.fontWeight.bold};
+		font-size: ${({ theme }) => theme.fontSize['xl']};
+		color: rgba(53, 56, 62, 0.5);
+		margin-left: 5px;
+	}
+`;
+
+const TodoHeader = styled.h1`
+	height: 47px;
+	width: 375px;
+	font-weight: ${({ theme }) => theme.fontWeight.bold};
+	font-size: ${({ theme }) => theme.fontSize['5xl']};
+`;
+
+const TodoItemWrapper = styled.div`
+	height: 500px;
+	overflow-y: auto;
+	-ms-overflow-style: none;
+	scrollbar-width: none;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
+
+const TodoInputWrapper = styled.div`
+	height: 145px;
 `;
