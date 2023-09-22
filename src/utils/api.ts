695c48063cfd7cbc7971a0329fe815d5fac493cc
jiyoon <vywns9978@gmail.com>
2023-09-22 17:31:00 +0900

    [✨ feat] Todo 관련 api 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,15 +1,36 @@
 import axiosInstance from './instance';
 
-type ApiProps = {
+type SignProps = {
 	email: string;
 	password: string;
 };
 
-const signupApi = ({ email, password }: ApiProps) => {
+type TodoProps = {
+	id: number;
+	todo: string;
+	isCompleted: boolean;
+};
+
+const signupApi = ({ email, password }: SignProps) => {
 	console.log(email, password);
 	return axiosInstance.post('/auth/signup', { email, password });
 };
 
-const signinApi = ({ email, password }: ApiProps) => axiosInstance.post('/auth/signin', { email, password });
+const signinApi = ({ email, password }: SignProps) => axiosInstance.post('/auth/signin', { email, password });
 
-export { signupApi, signinApi };
+const CreateTodoApi = (todo: string) => {
+	axiosInstance.post('/todos', { todo });
+};
+const GetTodosApi = () => {
+	axiosInstance.get('/todos');
+};
+
+const UpdateTodoApi = ({ id, todo, isCompleted }: TodoProps) =>
+	axiosInstance.put(`/todos/${id}`, {
+		todo,
+		isCompleted,
+	});
+
+const DeleteTodoApi = (id: number) => axiosInstance.delete(`/todos/${id}`);
+
+export { signupApi, signinApi, CreateTodoApi, GetTodosApi, UpdateTodoApi, DeleteTodoApi };
